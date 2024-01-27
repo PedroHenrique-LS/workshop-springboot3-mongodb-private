5fe73175155d2bf27bd59061f8a5fc6de4baafa3
Pedro Henrique <ph.laurindo2017@gmail.com>
2024-01-27 12:30:32 -0300

    conectando ao mongo com repository e service

---
+++ src/main/java/com/example/services/UserService.java
@@ -0,0 +1,24 @@
+package com.example.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.User;
+import com.example.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	
+
+}
